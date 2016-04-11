ad2a4b45c5b2d9ee2e2bd5c10182e86e37deb5ed
Allen Lin <allenlinli@gmail.com>
2016-04-11 22:51:08 +0800

    Done

--- Ferris Wheel/ShowInformationView.swift
+++ Ferris Wheel/ShowInformationView.swift
@@ -17,6 +17,7 @@ class ShowInformationView: UIView {
     var menuButton: UIButton!
     var whiteView: UIView!
     var orangeView: UIView!
+    var titleLabel: UILabel!
     var menuButtonDelegate: MenuButtonDelegate?
     let TopBarHeight: CGFloat = 50
     
@@ -28,24 +29,31 @@ class ShowInformationView: UIView {
         backgroundColor = UIColor(red: 74.0/255.0, green: 74.0/255.0, blue: 74.0/255.0, alpha: 1)
         contentView = UIView(frame: contentViewFrame)
         
-        whiteView = UIView(frame: CGRect(x: CGFloat(0.0),y: TopBarHeight,width: contentViewFrame.width,height: contentViewFrame.height-TopBarHeight))
+        whiteView = UIView(frame: CGRect(x: CGFloat(0.0),y: TopBarHeight+8,width: contentViewFrame.width,height: contentViewFrame.height-TopBarHeight))
         whiteView.backgroundColor = UIColor.whiteColor()
         whiteView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2/100))
         
-        orangeView = UIView(frame: CGRect(x: CGFloat(0.0),y: TopBarHeight,width: contentViewFrame.width,height: contentViewFrame.height-TopBarHeight))
+        orangeView = UIView(frame: CGRect(x: CGFloat(0.0),y: TopBarHeight+8,width: contentViewFrame.width,height: contentViewFrame.height-TopBarHeight))
         orangeView.backgroundColor = UIColor(red: 236.0/255.0, green: 97.0/255.0, blue: 51.0/255.0, alpha: 1)
         orangeView.transform = CGAffineTransformMakeRotation(CGFloat(-M_PI_2/100))
         
         let menuButtonImage = UIImage(named: "menubutton")
-        menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 47, height: 47))
+        menuButton = UIButton(frame: CGRect(x: 4, y: 4, width: 50, height: 50))
         menuButton.setImage(menuButtonImage, forState: UIControlState.Normal)
         menuButtonDelegate = delegate
         menuButton.addTarget(menuButtonDelegate!, action: #selector(ViewController.menuButtonTapped), forControlEvents: UIControlEvents.TouchDown)
+        
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: contentViewFrame.width, height: 50))
+        titleLabel.text = "Show Information"
+        titleLabel.textAlignment = NSTextAlignment.Right
+        titleLabel.textColor = UIColor(red: 173/255.0, green: 173/255.0, blue: 173/255.0, alpha: 1)
+        titleLabel.font = UIFont(name: titleLabel.font.fontName, size: 16)
         
         addSubview(contentView)
         contentView.addSubview(orangeView)
         contentView.addSubview(whiteView)
         contentView.addSubview(menuButton)
+        contentView.addSubview(titleLabel)
     }
     
     override init(frame: CGRect) {
--- Ferris Wheel/ViewController.swift
+++ Ferris Wheel/ViewController.swift
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 105.0/255.0, green: 122.0/255.0, blue: 138.0/255.0, alpha: 1)
+        
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.startPoint = CGPointZero
+        gradient.endPoint  = CGPoint(x: 1,y: 1)
+        let colorOfTopLeft = UIColor(red: 120.0/255.0,green: 136.0/255.0,blue: 152.0/255.0, alpha: 1)
+        let colorOfDownRight = UIColor(red: 57.0/255.0,green: 79.0/255.0,blue: 96.0/255.0, alpha: 1)
+        gradient.colors = [colorOfTopLeft.CGColor, colorOfDownRight.CGColor]
+        view.layer.insertSublayer(gradient, atIndex: 0)
         
         let wheelFrame = CGRect(x: view.center.x - WheelSize.width/2, y: view.center.y - WheelSize.height/2, width: WheelSize.width, height: WheelSize.height)
         ferrisWheel = FerrisWheel(frame:wheelFrame, delegate: self)
@@ -53,37 +61,29 @@ extension ViewController: CarriageDelegate, FerrisWheelDelegate, MenuButtonDeleg
     
     func carriageDidTapped(sender: Carriage?) {
         initialCarriageFrame = sender?.convertRect((sender?.bounds)!, toView: view)
+        print("view.bounds:\(view.bounds)")
         showInformationView = ShowInformationView(frame: initialCarriageFrame!, contentViewFrame: view.bounds, delegate: self)
-        guard let uShowInformationView = showInformationView else {
-            fatalError("no showInformationView")
-        }
         view.addSubview(showInformationView!)
-        
-//        //for testing
-//        uShowInformationView.backgroundColor = UIColor.orangeColor()
-//        print("showInformationView.frame:\(uShowInformationView.frame)")
         
         weak var wSelf = self
         let selfViewFrame = view.frame
-        UIView.animateWithDuration(5, delay: 1.0, options: UIViewAnimationOptions.TransitionNone, animations: { () -> Void in
+        UIView.animateWithDuration(0.7, delay: 1.0, options: UIViewAnimationOptions.TransitionNone, animations: { () -> Void in
             wSelf!.showInformationView!.frame = selfViewFrame
+            wSelf?.view.userInteractionEnabled = false
             }, completion: { (finished: Bool) -> Void in
-                print("In Completion------------------------------- ")
-                print("showInformationView.frame:\(uShowInformationView.frame)")
-                print("showInformationView.contentView.frame: \(uShowInformationView.contentView!.frame)")
-                print("showInformationView.menuButton.frame:\(uShowInformationView.menuButton.frame)")
-                print("wSelf.view.frame:\(wSelf!.view.frame)")
-//                print(UIWindow.screen.bouns)
+            wSelf?.view.userInteractionEnabled = true
         })
     }
     
     func menuButtonTapped() {
         print("menuButtonTapped")
         weak var wSelf = self
-        UIView.animateWithDuration(5, delay: 1.0, options: UIViewAnimationOptions.TransitionNone, animations: { () -> Void in
+        UIView.animateWithDuration(0.7, delay: 1.0, options: UIViewAnimationOptions.TransitionNone, animations: { () -> Void in
             wSelf!.showInformationView!.frame = wSelf!.initialCarriageFrame!
+            wSelf?.view.userInteractionEnabled = false
             }, completion: { (finished: Bool) -> Void in
                 wSelf!.showInformationView!.removeFromSuperview()
+                wSelf?.view.userInteractionEnabled = true
         })
     }
 }
